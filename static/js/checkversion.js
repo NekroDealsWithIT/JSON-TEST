7fa3cb5c42646775347e3ecaa55cebb0bc78d2b1
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2018-07-01 18:20:31 -0300

    Finalizo el chequeo de versiones
    Ya funciona el auto restart

--- static/js/checkversion.js
+++ static/js/checkversion.js
@@ -5,14 +5,12 @@ function versionCheck(){
 	//fetchVersion(window.location.href+"/../static/version");
 	fetchVersion("http://nekro-warframe.netlify.com/static/version");
 	if (version!=''&&fetchedVersion!=version){
-		//location.reload();	
-		console.log('voy a hacer reload');
-		console.log(version);
-		console.log(fetchedVersion);
+		location.reload();	
 	}else{
 		version=fetchedVersion;
-		console.log(version);
-		console.log(fetchedVersion);
+		if (version!=''){
+			ultimaVersion.innerHTML="<h3>Ultima version: "+version+"</h3>";
+		}
 	}
 }
 function fetchVersion(url){
@@ -33,46 +31,4 @@ fetch(url)
 	  fetchedVersion=html;
 	  console.log(doc);
 	})
-}
-
-function fetchVersion1(){
-	var url=window.location.href+"../static/version";
-	var request = new XMLHttpRequest();
-	request.open('GET', url);
-	request.responseType = 'json';
-	request.send();
-	request.onload = function() {
-	  version = request.response;
-	  console.log(version);
-	  //getVersion();
-	}
-}
-
-function fetchVersion2(){
-	var url="http://nekro-warframe.netlify.com/static/version";
-	console.log(url);
-	var request = new XMLHttpRequest();
-	request.open('GET', url);
-	request.responseType = 'json';
-	request.send();
-	request.onload = function() {
-	  version = request.response;
-	  console.log(version);
-	  //getVersion();
-	}
-}
-
-function getFileFromServer(url, doneCallback) {
-    var xhr;
-
-    xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = handleStateChange;
-    xhr.open("GET", url, true);
-    xhr.send();
-
-    function handleStateChange() {
-        if (xhr.readyState === 4) {
-            doneCallback(xhr.status == 200 ? xhr.responseText : null);
-        }
-    }
 }
