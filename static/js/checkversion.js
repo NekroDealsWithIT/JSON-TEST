c376472b06398de1726e3de39bcc63dda0901fe6
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2018-07-01 17:40:29 -0300

    Add files via upload

--- static/js/checkversion.js
+++ static/js/checkversion.js
@@ -1,6 +1,6 @@
 var version='';
 
-function getVersion(){
+function checkVersion(){
 	if (version!=''&&fetchedVersion!=version){
 		location.reload();	
 	}else{
@@ -8,8 +8,46 @@ function getVersion(){
 	}
 }
 
-function fetchVersion(){
+function versionCheck(){
+	fetchVersion("../static/version");
+}
+function fetchVersion(url){
+fetch(url)
+.then(function(response) {
+  // When the page is loaded convert it to text
+  version = response.text();
+  return response.text();
+})
+.then(function(html) {
+  // Initialize the DOM parser
+  var parser = new DOMParser();
+  // Parse the text
+  var doc = parser.parseFromString(html, "text/html");
+  
+  // You can now even select part of that html as you would in the regular DOM 
+  // Example:
+  // var docArticle = doc.querySelector('article').innerHTML;
+
+  console.log(doc);
+})
+}
+
+function fetchVersion1(){
 	var url=window.location.href+"../static/version";
+	var request = new XMLHttpRequest();
+	request.open('GET', url);
+	request.responseType = 'json';
+	request.send();
+	request.onload = function() {
+	  version = request.response;
+	  console.log(version);
+	  //getVersion();
+	}
+}
+
+function fetchVersion2(){
+	var url="http://nekro-warframe.netlify.com/static/version";
+	console.log(url);
 	var request = new XMLHttpRequest();
 	request.open('GET', url);
 	request.responseType = 'json';
