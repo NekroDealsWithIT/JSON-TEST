66be107e7121eab261ed9d2b6bd5b669e3e88a19
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2018-08-04 12:39:31 -0300

    Agrego funcion stringToSlug

--- static/js/tools.js
+++ static/js/tools.js
@@ -332,6 +332,26 @@ function strReplaceAllNonPrintable(texto,reemplazarPor=' '){
 	return texto;
 }
 
+function stringToSlug(str) {
+	str = str.replace(/^\s+|\s+$/g, ""); // trim
+	str = str.toLowerCase();
+
+	// remove accents, swap ñ for n, etc
+	var from = 	"åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+	var to = 	"aaaaaaeeeeiiiioooouuuunc------";
+
+	for (var i = 0, l = from.length; i < l; i++) {
+		str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+	}
+
+	str = str
+	.replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+	.replace(/\s+/g, "-") // collapse whitespace and replace by -
+	.replace(/-+/g, "-"); // collapse dashes
+
+	return str;
+}
+
 /*
 -----------------------------------------------------
 	Funciones de tiempo
